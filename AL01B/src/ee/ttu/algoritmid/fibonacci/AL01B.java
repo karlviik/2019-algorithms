eadfc091d2a0716ab7dabdac15a775b27b0fe654
karlviik <viik.karl2@gmail.com>
2019-09-04 14:30:01 +0300

    Initial solution

--- AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
+++ AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
@@ -1,17 +1,40 @@
 package ee.ttu.algoritmid.fibonacci;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
+
+import static java.lang.System.nanoTime;
 
 public class AL01B {
+
+  public static BigInteger iterativeF(int n) {
+    if (n <= 1) {
+      return BigInteger.valueOf(n);
+    }
+    BigInteger a = BigInteger.ZERO;
+    BigInteger b = BigInteger.ONE;
+    BigInteger c = BigInteger.ZERO;
+    for (int i = 2; i <= n; i++) {
+      c = a.add(b);
+      a = b;
+      b = c;
+    }
+    return c;
+  }
 
   /**
    * Estimate or find the exact time required to compute the n-th Fibonacci number.
    * @param n The n-th number to compute.
    * @return The time estimate or exact time in YEARS.
    */
-  public String timeToComputeRecursiveFibonacci(int n) {
-    // TODO
-    return "";
+  public static String timeToComputeRecursiveFibonacci(int n) {
+    long measureStart = System.nanoTime();
+    recursiveF(12);
+    long measureTime = System.nanoTime() - measureStart;
+    long lineTime = measureTime / (3 * 89 - 2); // 3 * F(10) - 2
+    BigInteger lineCount = iterativeF(n).multiply(BigInteger.valueOf(3)).subtract(BigInteger.valueOf(2));
+    return BigDecimal.valueOf(lineTime).multiply(new BigDecimal(lineCount)).divide(BigDecimal.valueOf(100000000.0 * 60 * 60 * 24 * 365), 18, RoundingMode.UP).toString();
   }
 
   /**
@@ -20,9 +43,13 @@ public class AL01B {
    * @param n The n-th number to compute.
    * @return The n-th Fibonacci number as a string.
    */
-  public BigInteger recursiveF(int n) {
+  public static BigInteger recursiveF(int n) {
     if (n <= 1)
       return BigInteger.valueOf(n);
     return recursiveF(n - 1).add(recursiveF(n - 2));
+  }
+
+  public static void main(String[] args) {
+    System.out.println(timeToComputeRecursiveFibonacci(69));
   }
 }
