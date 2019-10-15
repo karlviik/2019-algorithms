8051727d88bd1ad28bbf0abbfc7340847603fc8f
karlviik <viik.karl2@gmail.com>
2019-10-15 18:57:07 +0300

    Initial

---
+++ AL04/src/ee/ttu/algoritmid/scoreboard/ScoreBoard.java
@@ -0,0 +1,25 @@
+package ee.ttu.algoritmid.scoreboard;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+public class ScoreBoard {
+
+    private PriorityQueue<Participant> pq = new PriorityQueue<>(Comparator.comparing(Participant::getTime));
+    /**
+     * Adds a participant's time to the checkpoint scoreboard
+     */
+    public void add(Participant participant) {
+        pq.add(participant);
+    }
+
+    /**
+     * Returns top n number of participants in the checkpoint to be displayed on the scoreboard
+     * This method will be queried by the tests every time a new participant is added
+     */
+    public List<Participant> get(int n) {
+        return pq.stream().limit(n).collect(Collectors.toList());
+    }
+}
