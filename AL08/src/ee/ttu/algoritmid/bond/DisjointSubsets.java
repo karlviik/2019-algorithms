c444db05f544bdf27ec46e5292432882389281b8
karlviik <viik.karl2@gmail.com>
2019-12-10 15:46:19 +0200

    Cleanup

--- AL08/src/ee/ttu/algoritmid/bond/DisjointSubsets.java
+++ AL08/src/ee/ttu/algoritmid/bond/DisjointSubsets.java
@@ -26,27 +26,8 @@ public class DisjointSubsets {
 		if (!elementMapper.containsKey(element1) || !elementMapper.containsKey(element2)) {
 			throw new IllegalArgumentException();
 		}
-		Node node1 = elementMapper.get(element1);
-		ArrayList<Node> optimization = new ArrayList<>();
-
-		while (!node1.parent.equals(node1)) {
-			optimization.add(node1);
-
-			node1 = node1.parent;
-		}
-		for (Node subnode : optimization) {
-			subnode.parent = node1;
-		}
-		Node node2 = elementMapper.get(element2);
-		optimization.clear();
-		while (!node2.parent.equals(node2)) {
-			optimization.add(node1);
-
-			node2 = node2.parent;
-		}
-		for (Node subnode : optimization) {
-			subnode.parent = node2;
-		}
+		Node node1 = elementMapper.get(find(element1));
+		Node node2 = elementMapper.get(find(element2));
 		if (node1.name.equals("U") || node1.name.equals("A")) {
 			node2.parent = node1;
 		}
